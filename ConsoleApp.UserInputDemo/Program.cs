5a9ff468f4ab61b5ed2195cba7dbc042abf00d13
JoannieCote <2460886@cegepgim.ca>
2025-10-18 11:17:37 -0400

    Add project files.

---
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml.Linq;
+
+namespace ConsoleApp.UserInputDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables
+
+            string firstName = string.Empty;
+            string lastName = string.Empty;
+            int age = 0;
+            int retirementAge = 65;
+
+            // Prompt the user for input
+
+            Console.WriteLine("Please entre your first name");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Please entre your name");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Please entre your age");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            // Process the data
+
+            int workingYearsRemaining = retirementAge - age;
+
+            // Output the result to the user
+
+            Console.WriteLine($"Full name: {firstName} {lastName}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Working years remaining: {workingYearsRemaining}");
+
+        }
+    }
+}
---
+++ ConsoleApp.VariableDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp.VariableDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Trevoir Wiliams";
+            string name2 = "Joannie Côté";
+            Console.WriteLine(name);
+            Console.WriteLine($"Hello, World! {name}");
+            Console.WriteLine("I am " + name);
+            Console.WriteLine("I was given the name {0} and my name is {1}", name,name2);
+
+            int age = 39;
+            Console.WriteLine(age);
+            int retirementYearsLeft = 11;
+            int retirementAge = age + retirementYearsLeft;
+
+            Console.WriteLine(retirementAge);
+            Console.WriteLine(age + retirementYearsLeft);
+
+            bool isRetired = false;
+        } 
+    }
+}
