12a62eab44dfce416416fc7c417b0d3d84911a85
JoannieCote <2460886@cegepgim.ca>
2025-10-18 12:43:33 -0400

    .

--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -12,17 +12,30 @@ namespace ConsoleApp.UserInputDemo
             string lastName = string.Empty;
             int age = 0;
             int retirementAge = 65;
+            decimal salary = 0;
+            char gender = char.MinValue;
+            bool working = false;
+
 
             // Prompt the user for input
 
-            Console.WriteLine("Please entre your first name");
+            Console.Write("Please entre your first name: ");
             firstName = Console.ReadLine();
 
-            Console.WriteLine("Please entre your name");
+            Console.Write("Please entre your last name: ");
             lastName = Console.ReadLine();
 
-            Console.WriteLine("Please entre your age");
+            Console.Write("Please enter your age: ");
             age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter your salary: ");
+            salary = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Please enter your gender (M ou F)");
+            gender = Convert.ToChar(Console.ReadLine());
+                
+            Console.Write("Are you working? (true ou false)");
+            working = Convert.ToBoolean(Console.ReadLine());
 
             // Process the data
 
@@ -32,8 +45,10 @@ namespace ConsoleApp.UserInputDemo
 
             Console.WriteLine($"Full name: {firstName} {lastName}");
             Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Your salary is: {salary}");
+            Console.WriteLine($"Your gender is: {gender}");
+            Console.WriteLine($"You are Employed: {working}");
             Console.WriteLine($"Working years remaining: {workingYearsRemaining}");
-
         }
     }
 }
